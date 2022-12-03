c5f046eaac16d217f1e83d48ed224caf6bf2b6af
ashish khose <100353863+Ashishkhose7@users.noreply.github.com>
2022-12-03 18:16:46 +0530

    Update firstPage.js

--- Project(firstPage)/firstPage.js
+++ Project(firstPage)/firstPage.js
@@ -1,4 +1,57 @@
+// Coupen Code
+setTimeout(loadCoupen,5000)
+function loadCoupen(){
+    document.getElementById('coupen-code').style.visibility='visible';
+    document.getElementById('main').style.opacity='0.5';
+}
+function closeCoupen(){
+    document.getElementById('coupen-code').style.visibility='hidden';
+    document.getElementById('main').style.opacity='1';
+}
 
+
+//Added to cart 
+let addBtn = document.getElementsByClassName("add-btn")
+let showMsg = document.getElementsByClassName("add-cart")
+
+for (const key in addBtn) {
+    addBtn[key].onclick = ()=>{
+        showMsg[key].style.visibility='visible'
+        setTimeout(()=>{
+            showMsg[key].style.visibility='hidden'
+        },2000)
+    }
+}
+
+// Location with Weather
+let weathertag = document.getElementById("weather-details");
+
+function getweather(){
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(showPosition)
+}
+else{
+    console.error("Geolocation is not supported by this browser")
+}
+}
+
+function showPosition(data){
+    // console.log(data)
+    let lat = data.coords.latitude;
+    let long = data.coords.longitude;
+    // console.log(lat, long)
+    let url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+    
+    fetch(url,{method:'GET'})
+       .then(response => response.json())
+       .then((data) => {
+           let cityName = data.city.name;
+           let cityTemp = data.list[0].temp.day;
+           weathertag.innerHTML=`${cityName}  ${cityTemp}°C`
+       })
+}
+
+// Dark and light mode
 function darkMode() {
     document.body.classList.toggle("body-dark-mode");
     let dark = document.getElementById("dark-mode-icon");
@@ -9,7 +62,7 @@ function darkMode() {
 
     if (dark.style.color === "white") {
         dark.style.color = "yellow";
-        darkText[0].innerHTML="Normal Mode";
+        darkText[0].innerHTML="Dark Mode";
         darkNav[0].style.backgroundColor = "darkblue";
         darkSection[0].style.backgroundColor = "midnightblue";
         for (const dhead of darkHeading) {
@@ -18,7 +71,7 @@ function darkMode() {
     }
     else {
         dark.style.color = "white";
-        darkText[0].innerHTML="Dark Mode";
+        darkText[0].innerHTML="Normal Mode";
         darkNav[0].style.backgroundColor = "black";
         darkSection[0].style.backgroundColor = "dimgray";
         for (const dhead of darkHeading) {
@@ -26,3 +79,4 @@ function darkMode() {
         }
     }
 }
+
